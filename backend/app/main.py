80fa0223cd387cc19786118897b87235135f1931
Anjireddy Medam <anjireddy495521@gmail.com>
2026-08-29 13:41:17 +0530

    Allow production frontend in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,13 +21,13 @@ Base.metadata.create_all(bind=engine)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://anjireddytech.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # Register contact router
 app.include_router(contact_router)
